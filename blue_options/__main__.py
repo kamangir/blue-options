0482fe539992872f0d4c96e50a9f0f4ed2641585
kamangir <arash@kamangir.net>
2024-07-02 20:41:28 -0700

    refactor - kamangir/bolt#746

--- blue_options/__main__.py
+++ blue_options/__main__.py
@@ -1,9 +1,6 @@
 from blue_options import NAME, VERSION, DESCRIPTION, ICON
-from blue_options.logger import logger
 from blueness.argparse.generic import main
 
 success, message = main(__file__, NAME, VERSION, DESCRIPTION, ICON)
 if not success:
-    logger.error(message)
-
-
+    print(f"❗️ {message}")
--- blue_options/node/__init__.py
+++
@@ -1,5 +0,0 @@
-from blue_options import NAME
-
-NAME = f"{NAME}.node"
-
-
