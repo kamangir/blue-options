421b2fc871353c355bad5a449c2f58f1106eba26
kamangir <arash@kamangir.net>
2024-09-09 21:47:25 -0700

    += tests - kamangir/bolt#746

--- blue_options/__init__.py
+++ blue_options/__init__.py
@@ -6,7 +6,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} an options argument for Bash."
 
-VERSION = "4.51.1"
+VERSION = "4.52.1"
 
 REPO_NAME = "blue-options"
 
---
+++ blue_options/tests/test_logger.py
@@ -0,0 +1,5 @@
+from blue_options.logger import logger
+
+
+def test_logger():
+    logger.info("testing")
