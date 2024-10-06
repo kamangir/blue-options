21adf31aea16079a54a45ca56e0e0991b7094367
kamangir <arash@kamangir.net>
2024-10-06 16:23:42 -0700

    duplicate test removed - kamangir/bolt#746

--- blue_options/__init__.py
+++ blue_options/__init__.py
@@ -6,7 +6,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} options for Bash."
 
-VERSION = "4.87.1"
+VERSION = "4.88.1"
 
 REPO_NAME = "blue-options"
 
--- blue_options/tests/test.py
+++
@@ -1,5 +0,0 @@
-from blue_options import fullname
-
-
-def test_fullname():
-    assert fullname()
