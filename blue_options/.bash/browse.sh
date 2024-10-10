79ccaa94e96c6a56d001ffa9fa7626fbb5afe4f6
kamangir <arash@kamangir.net>
2024-10-09 17:16:34 -0700

    += browse - kamangir/bolt#746

---
+++ blue_options/.bash/browse.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env bash
+
+function abcli_browse() {
+    local url=$1
+    local description=$2
+
+    if [[ "$url" == "help" ]]; then
+        abcli_show_usage "@browse <url>" \
+            "browse <url>."
+        return
+    fi
+
+    [[ ! -z "$description" ]] &&
+        abcli_log "$description"
+
+    abcli_log "🔗 $url"
+    if [ "$abcli_is_mac" == true ]; then
+        open "$url"
+    elif [ "$abcli_is_ec2" == true ]; then
+        firefox "$url"
+    elif [ "$abcli_is_rpi" == true ]; then
+        DISPLAY=:0 chromium-browser -kiosk --no-sandbox "$url"
+    else
+        abcli_log_warning "-@browse: $url: skipped."
+    fi
+}
