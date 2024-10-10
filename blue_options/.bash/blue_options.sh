f5f4e2e2ad9e1b88f3e545420923e0180a1dc076
kamangir <arash@kamangir.net>
2024-10-09 19:57:42 -0700

    += eval - kamangir/bolt#746

--- blue_options/.bash/blue_options.sh
+++ blue_options/.bash/blue_options.sh
@@ -26,6 +26,7 @@ source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/source.sh
 
 source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/browse.sh
 source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/env.sh
+source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/eval.sh
 source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/generic_task.sh
 source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/install.sh
 source $(dirname "$(realpath "${BASH_SOURCE[0]}")")/list.sh
--- blue_options/__init__.py
+++ blue_options/__init__.py
@@ -6,7 +6,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} options for Bash."
 
-VERSION = "4.100.1"
+VERSION = "4.101.1"
 
 REPO_NAME = "blue-options"
 
