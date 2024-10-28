ef81cb4ddc407ce33ca8b97cc4929a6d2721f83e
kamangir <arash@kamangir.net>
2024-10-27 22:50:14 -0700

    help refactor - kamangir/bolt#746

--- blue_options/.bash/help.sh
+++ blue_options/.bash/help.sh
@@ -2,6 +2,11 @@
 
 function abcli_help() {
     local callable=$1
+    if [[ -z "$callable" ]]; then
+        abcli_version
+        return
+    fi
+
     if alias "$callable" &>/dev/null; then
         callable=$(alias "$callable" | sed -E "s/^alias $callable='(.*)'$/\1/")
     fi
--- blue_options/__init__.py
+++ blue_options/__init__.py
@@ -6,7 +6,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} options for Bash."
 
-VERSION = "4.137.1"
+VERSION = "4.138.1"
 
 REPO_NAME = "blue-options"
 
