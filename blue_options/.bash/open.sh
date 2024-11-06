a86b2c3c9f895f175a35a30c3ee7cf5c4cb1ddce
kamangir <arash@kamangir.net>
2024-11-05 19:01:10 -0800

    help refactor - kamangir/bolt#746

--- blue_options/.bash/open.sh
+++ blue_options/.bash/open.sh
@@ -3,17 +3,10 @@
 function abcli_open() {
     local options=$1
 
-    if [ $(abcli_option_int "$options" help 0) == 1 ]; then
-        options="extension=<extension>,filename=<filename>,QGIS"
-        abcli_show_usage "@open$ABCUL[$options]$ABCUL[.|<object-name>]" \
-            "open <object-name>."
-        return
-    fi
-
-    local object_name=$(abcli_clarify_object $2 .)
-
     local extension=$(abcli_option "$options" extension)
     [[ $(abcli_option_int "$options" QGIS 0) == 1 ]] && extension="qgz"
+
+    local object_name=$(abcli_clarify_object $2 .)
 
     local filename=$(abcli_option "$options" filename)
     [[ ! -z "$extension" ]] &&
