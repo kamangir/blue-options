8a2def77457d13507ef558ed202a3eafde8c7c15
kamangir <arash@kamangir.net>
2024-11-24 19:23:37 -0800

    @badge refactor - kamangir/bolt#746

--- blue_options/.bash/terminal.sh
+++ blue_options/.bash/terminal.sh
@@ -4,12 +4,6 @@ function abcli_badge() {
     local note=$1
     [[ "$note" == "clear" ]] && note=""
 
-    if [[ "$note" == "help" ]]; then
-        abcli_show_usage "@badge <note>" \
-            "badge <note>."
-        return
-    fi
-
     echo -e "\033]1337;SetBadgeFormat=$(echo -n "$note" | base64)\a"
 }
 
